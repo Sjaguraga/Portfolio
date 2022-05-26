a6e2386d12345735c2c18589985304b1aa9e6c6b
serref jaguraga <reef163@gmail.com>
2022-05-26 13:57:09 -0400

    created skills page

---
+++ src/components/Skills.js
@@ -0,0 +1,70 @@
+import React from "react";
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import github from "../assets/github.png";
+import javascript from "../assets/javascript.png";
+import node from "../assets/node.png";
+import react from "../assets/react.png";
+import rails from "../assets/rails.png";
+import postgresql from "../assets/postgresql.png";
+
+function Skills() {
+  return (
+    <div name="skills" className="bg-[#0a192f] text-gray-300">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-500 text-gray-300 ">
+            Experience
+          </p>
+          <p className="py-4">// These are the technologies I've worked with</p>
+        </div>
+
+        <div className="w-full grid grild-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={html} alt="HTML Icon" />
+            <p className="my-4">HTML</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={css} alt="CSS Icon" />
+            <p className="my-4">CSS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={github} alt="GITHUB Icon" />
+            <p className="my-4">GITHUB</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src={javascript}
+              alt="JAVASCRIPT Icon"
+            />
+            <p className="my-4">JAVASCRIPT</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={node} alt="NODE Icon" />
+            <p className="my-4">NODE</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={react} alt="REACT Icon" />
+            <p className="my-4">REACT</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={rails} alt="RAILS Icon" />
+            <p className="my-4">RAILS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src={postgresql}
+              alt="POSTGRESQL Icon"
+            />
+            <p className="my-4">POSTGRESQL</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
