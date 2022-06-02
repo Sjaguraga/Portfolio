cd8a623a3608d90089e844ea4d0f285cfdb56fb2
serref jaguraga <reef163@gmail.com>
2022-06-02 01:23:33 -0400

    css changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,7 @@ function Contact() {
         className="flex flex-col max-w-[600px] w-full"
       >
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-green-500 text-gray-300">
+          <p className="text-4xl font-bold inline border-b-4 border-[#4ab5bd] text-gray-300">
             Contact
           </p>
           <p className=" text-gray-300 py-4">
@@ -37,7 +37,7 @@ function Contact() {
           rows="10"
           placeholder="Message"
         ></textarea>
-        <button className="text-white border-2 hover:bg-red-500 hover:border-yellow-100 px-4 py-3 my-8 mx-auto flex items-center">
+        <button className="text-white border-2 hover:bg-[#4ab5bd] hover:border-[#4ab5bd] px-4 py-3 my-8 mx-auto flex items-center">
           Let's Collaborate
         </button>
       </form>
