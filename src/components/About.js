60497573eb53149854bde745903b3bbc90dde7f4
serref jaguraga <reef163@gmail.com>
2022-06-02 20:04:13 -0400

    done

--- src/components/About.js
+++ src/components/About.js
@@ -19,10 +19,10 @@ function About() {
           <div>
             <p>
               I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you
-              have a software expert available at your fingertips?
+              the lives of those around me. I am looking to create software for
+              clients ranging from individuals and small-businesses all the way
+              to large enterprise corporations. What would you do if you have a
+              software expert available at your fingertips?
             </p>
           </div>
         </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -17,7 +17,9 @@ function Skills() {
           <p className="text-4xl font-bold inline border-b-4 border-[#4ab5bd] text-gray-300 ">
             Skills
           </p>
-          <p className="py-4">These are the technologies I've worked with</p>
+          <p className="py-4">
+            Here are some of the technologies I've worked with
+          </p>
         </div>
 
         <div className="w-full grid grild-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
